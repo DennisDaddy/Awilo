8a0eb45d965eaad44ccd098f4346488f8d8c1811
DennisDaddy <softteez95@gmail.com>
2018-09-21 17:26:15 +0300

    Retrieve all questions

--- app/api.py
+++ app/api.py
@@ -3,14 +3,33 @@ import sys
 from flask import Flask, jsonify, request, make_response
 from flask_restful import Resource, Api
 
+from app.models import *
 app = Flask(__name__)
 api = Api(app)
 
 class Home(Resource):
+    """"This is a class for root endpoint"""
     def get(self):
         return jsonify({'message': 'Welcome to Stackoverflow-lite'})
 
-api.add_resource(Home, '/api/v1')
+class QuestionList(Resource):
+    """This is a class for questions without IDs"""
+    def get(self):
+        my_list = []
+        try:
+            cur.execute("SELECT * FROM questions")
+            rows = cur.fetchall()
+            for row in rows:
+                id = row[0]
+                title = row[1]
+                content = row[2]
+                my_list.append({"id":id, "title":title, "content":content})
+        except:
+            return jsonify({"message": "cannot retrieve questions"})
+        return jsonify({"rows": my_list})
 
+api.add_resource(Home, '/api/v1')
+api.add_resource(QuestionList, '/api/v1/questions', endpoint='questions')
+# api.add_resource(Question, '/api/v1/questions/<int:id>', endpoint='question')
 if __name__ == '__main__':
     app.run(debug=True)
