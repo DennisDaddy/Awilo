96283145b25d81eb1c5c61be4e7ce068b0ff52ee
DennisDaddy <softteez95@gmail.com>
2018-09-21 16:59:47 +0300

    Create model and add tables

---
+++ app/models.py
@@ -0,0 +1,37 @@
+"""Import flask modules"""
+import psycopg2
+
+#connect using psycopg
+conn = psycopg2.connect("dbname=stackover user=postgres password=123456 host=localhost")
+#Activate connection cursor
+cur = conn.cursor()
+
+#create tables
+cur.execute('''CREATE TABLE IF NOT EXISTS questions(
+    id serial PRIMARY KEY,
+    title varchar (50) NOT NULL,
+    content varchar (100) NOT NULL,
+    timestamp timestamp default current_timestamp
+) ''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS users(
+    id serial PRIMARY KEY,
+    username varchar (50) NOT NULL,
+    email varchar (100) NOT NULL,
+    password varchar (100) NOT NULL,
+    password_confirmation varchar (100) NOT NULL,
+    timestamp timestamp default current_timestamp
+) ''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS answers(
+    id serial PRIMARY KEY,
+    body varchar (100) NOT NULL,
+    timestamp timestamp default current_timestamp
+) ''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS comments(
+    id serial PRIMARY KEY,
+    content varchar (100) NOT NULL,
+    timestamp timestamp default current_timestamp
+) ''')
+conn.commit()
