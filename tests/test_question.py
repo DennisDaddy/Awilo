1c5087f8ad75f568dba10e1dea2806c4be05c4d4
DennisDaddy <softteez95@gmail.com>
2018-09-22 15:11:50 +0300

    Update README file

--- tests/test_question.py
+++ tests/test_question.py
@@ -5,6 +5,16 @@ import os
 import sys
 from app import *
 sys.path.insert(0, os.path.abspath(".."))
+
 class QuestionsTestCase(unittest.TestCase):
+    def setUp(self):
+		"""Define test variables and initialize the app."""
+		self.app = create_app(config_name="testing")
+		self.client = self.app.test_client
+
     def test_home(self):
-        pass
+		"""This method tests root endpoint"""
+		tester = app.test_client(self)
+		response = tester.get('/api/v1', content_type='application/json')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn(b'Welcome to Stackoverflow-lite', response.data)
